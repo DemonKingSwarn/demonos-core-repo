8bc4c9d0eff096525241d17462227a1102fe2add
DemonKingSwarn <rockingswarn@gmail.com>
2022-05-22 17:46:43 +0530

    Updating Database.

--- buid-db.sh
+++ buid-db.sh
@@ -2,3 +2,7 @@
 
 cd ./x86_64
 repo-add demonos-core-repo.db.tar.gz *.pkg.tar.zst
+rm -rf *.db
+rm -rf *.files
+mv demonos-core-repo.db.tar.gz demonos-core-repo.db
+mv demonos-core-repo.files.tar.gz demonos-core-repo.files
